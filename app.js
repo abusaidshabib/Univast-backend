90aea4e341a9cd8b0bb472851172938119894073
abusaidshabib <abusaidshabib712@gmail.com>
2023-08-19 03:14:51 +0600

    cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const globalErrorHandler = require("./controllers/errorController");
 const admissionRouter = require("./routes/admissionRouter");
@@ -14,6 +15,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
