b641b04547f03dea5d4c3a707aceab53b9989113
abusaidshabib <abusaidshabib712@gmail.com>
2023-08-28 02:54:52 +0600

    student done

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,4 +1,5 @@
 const Student = require("../models/studentModel");
+const { studentIdCreator } = require("../subControllers/studentSub");
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 const {
@@ -12,19 +13,28 @@ const {
 exports.getStudents = catchAsync(async (req, res, next) => {
   const queryKeys = Object.keys(req.query);
   if (queryKeys.length === 1) {
-    const newStudent = await Student.findOne(req.query);
+    const newStudent = await Student.find(req.query);
     dataGetResponse(res, newStudent);
   } else if (queryKeys.length === 0) {
     const newStudent = await Student.find();
     dataGetResponse(res, newStudent);
-  } else {
+  } else if (queryKeys.length > 1) {
     serverNOTdeclared(res);
   }
 });
 
 exports.createStudent = catchAsync(async (req, res, next) => {
-  const result = await Student.create(req.body);
-  sendCreatedResponse(res, result);
+  let result = [];
+  let bodyData = req.body;
+  if (req.body.studentId) {
+    customResponse(res, 404, result, "Student Id not creatable");
+  } else {
+    const collectionLength = await Student.countDocuments();
+    const studentId = studentIdCreator(collectionLength);
+    bodyData.studentId = studentId;
+    const result = await Student.create(bodyData);
+    sendCreatedResponse(res, result);
+  }
 });
 
 exports.updateStudent = catchAsync(async (req, res, next) => {
---
+++ subControllers/studentSub.js
@@ -0,0 +1,6 @@
+exports.studentIdCreator = (index) => {
+  const yourDateString = "2023-08-22";
+  const dateObject = new Date(yourDateString);
+  const year = dateObject.getFullYear();
+  return year.toString() + (index + 1).toString();
+};
