80bbf155b99c91b53459ae7190f6c4a0166e757b
abusaidshabib <abusaidshabib712@gmail.com>
2023-09-05 17:17:36 +0600

    prog

--- controllers/programController.js
+++ controllers/programController.js
@@ -12,7 +12,7 @@ const AppError = require("../utils/AppError");
 exports.getProgram = catchAsync(async (req, res, next) => {
   const queryKeys = Object.keys(req.query);
   if (queryKeys.length === 1) {
-    const result = await Program.findOne(req.query);
+    const result = await Program.find(req.query);
     dataGetResponse(res, result);
   } else if (queryKeys.length === 0) {
     const result = await Program.find();
