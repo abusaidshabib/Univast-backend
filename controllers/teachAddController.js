45d18d986c2c1310db52efa3ffd6bcc9c86f39d3
abusaidshabib <abusaidshabib712@gmail.com>
2023-09-05 21:47:45 +0600

    done

--- controllers/teachAddController.js
+++ controllers/teachAddController.js
@@ -3,6 +3,5 @@ const { sendCreatedResponse } = require("../utils/successStatus");
 
 exports.CreateTechAdd = catchAsync(async (req, res, next) => {
   console.log("working");
-  let result = [];
   sendCreatedResponse(res, result);
 });
