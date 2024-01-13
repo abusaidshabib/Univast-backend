6dda33916a6a398ddd14722c4edd7ed56957ff3d
abusaidshabib <abusaidshabib712@gmail.com>
2024-01-13 18:26:03 +0600

    done

--- models/courseExtensionModel.js
+++ models/courseExtensionModel.js
@@ -1,18 +1,42 @@
 const { default: mongoose } = require("mongoose");
 
+const noticeSchema = new mongoose.Schema({
+  url: String,
+  title: String
+})
+
+const classworkSchema = new mongoose.Schema({
+  url: String,
+  title: String,
+});
+
+const outlineSchema = new mongoose.Schema({
+  url: String,
+  title: String,
+});
+
+const lecturesSchema = new mongoose.Schema({
+  url: String,
+  title: String,
+});
+
 const courseExtensionSchema = new mongoose.Schema({
   semester: String,
   courseId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Course",
-        unique: true,
-      },
-  notice: [String],
-  classwork: [String],
-  outline: [String],
-  lectures: [String],
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Course",
+  },
+  notice: [noticeSchema],
+  classwork: [classworkSchema],
+  outline: [outlineSchema],
+  lectures: [lecturesSchema],
 });
 
-const CourseExtension = mongoose.model("CourseExtension", courseExtensionSchema);
-courseExtensionSchema.index({ semester: 1 }, { unique: true });
+courseExtensionSchema.index({ semester: 1, courseId: 1 }, { unique: true });
+
+const CourseExtension = mongoose.model(
+  "CourseExtension",
+  courseExtensionSchema
+);
+
 module.exports = CourseExtension;
