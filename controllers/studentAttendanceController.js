9f8eb0a7be6bb299940aae63492259e154b58509
abusaidshabib <abusaidshabib712@gmail.com>
2024-01-14 20:57:58 +0600

    done

--- controllers/studentAttendanceController.js
+++ controllers/studentAttendanceController.js
@@ -30,15 +30,51 @@ exports.postStudentAttendance = catchAsync(async (req, res, next) => {
   
       await newAttendance.save();
     }
-    new ResponseGenerator(res, 200, 'Attendance updated successfully');
+    new ResponseGenerator(res, 201, 'Attendance updated successfully');
   });
 
-  exports.getStudentAttendance = catchAsync(async(req, res, next) => {
-    let result;
-    let {courseCode, semester, date} = req.query;
-    result = await Student.find({
+  exports.getStudentAttendance = catchAsync(async (req, res, next) => {
+    let statusCode = 200;
+    let { courseCode, semester, date } = req.query;
+  
+    try {
+      const enrolledStudents = await Student.find({
         "courses_taught.semester": semester,
         "courses_taught.courseCode": courseCode,
       });
-    res.json({})
-  })
+
+      let course = await Course.findOne({courseCode: courseCode})
+  
+      const attendanceRecords = await Student_Attendance.find({
+        "semester": semester,
+        "date": new Date(date),
+        "course": course._id,
+      });
+  
+      const attendanceMap = new Map();
+
+      attendanceRecords.forEach((record) => {
+        attendanceMap.set(record.student.toString(), record.status);
+      });
+  
+      const result = enrolledStudents.map((student) => {
+        const student_Id = student._id.toString();
+        const studentId = student.studentId;
+        const studentName = `${student.personal.firstName} ${student.personal.lastName}`;
+        const attendanceStatus = attendanceMap.has(student_Id)
+          ? attendanceMap.get(student_Id)
+          : false;
+  
+        return {
+          student_name: studentName,
+          status: attendanceStatus,
+          studentId
+        };
+      });
+  
+      new ResponseGenerator(res, statusCode, result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json(error);
+    }
+  });
