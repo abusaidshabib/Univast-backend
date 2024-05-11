48b7444fda8dbabd5fced386180d63e6f4fb1dda
abusaidshabib <abusaidshabib712@gmail.com>
2024-05-11 22:03:49 +0600

    course done

--- controllers/resultsController.js
+++ controllers/resultsController.js
@@ -19,3 +19,30 @@ exports.createStudentResult = async (req, res) => {
       res.status(500).json({ error: 'Internal server error' });
     }
   };
+
+exports.updateStudentResult = async (req, res) => {
+  try{
+    const {studentId, semester, courseCode} = req.params;
+    const newResult = req.body;
+    const student = await Student.findOne({studentId});
+    if (!student){
+      return res.status(404).json({error: 'Student not found'})
+    }
+    const resultIndex = student.results.findIndex(result => result?.semester === semester && result?.courseCode === courseCode);
+
+    if (resultIndex === -1){
+      return res.status(404).json({error: 'Result not found for the given semester and course code'});
+    }
+
+    console.log(newResult)
+
+    student.results[resultIndex] = newResult;
+    await student.save()
+
+    res.status(200).json({ message: 'Result updated successfully', student });
+  }
+  catch (error){
+    console.error(error);
+    res.status(500).json({error: 'Internal server error'});
+  }
+}
--- routes/resultRouter.js
+++ routes/resultRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { createStudentResult } = require("../controllers/resultsController");
+const { createStudentResult, updateStudentResult } = require("../controllers/resultsController");
 const router = express.Router();
 
-router.route("/:studentId").post(createStudentResult);
+// router.route("/:studentId").post(createStudentResult);
+router.route("/:studentId/:semester/:courseCode").put(updateStudentResult);
 
 module.exports = router;
