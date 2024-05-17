efffc7924bb0725c8d00ef96f28476f04a9d22a4
abusaidshabib <abusaidshabib712@gmail.com>
2024-05-17 17:49:13 +0600

    resutl

--- models/resultModel.js
+++ models/resultModel.js
@@ -1,22 +1,47 @@
 const mongoose = require("mongoose");
+const Course = require("./courseModel");
 
 const resultSchema = new mongoose.Schema({
     semester: String,
+    course:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+    },
     courseCode: String,
     grade: Number,
     mid:{
+        grade: String,
         number: Number
     },
     final:{
+        grade: String,
         number: Number
     },
     attendance: {
+        grade: String,
         number: Number
     },
     assessment:{
+        grade: String,
         number: Number
     }
 });
+
+resultSchema.pre('save', async function(next) {
+    try {
+        if (this.isModified('courseCode')) {
+            const course = await Course.findOne({ courseCode: this.courseCode });
+            if (!course) {
+                throw new Error('Course not found for the given courseCode');
+            }
+            this.course = course._id;
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
 
 const Result = mongoose.model("Result", resultSchema);
 module.exports = { Result, resultSchema };
